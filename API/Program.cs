93d87e9a626ad18d897327cd31308b8819ff6a31
Oleg Nekrasov <79853886592@yandex.ru>
2023-02-04 20:38:14 +0300

    Added NuGet package "Swashbuckle.AspNetCore"

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -13,5 +15,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
