d43e0a7ebb011e161264c7b76fa8ec9774640f74
Oleg Nekrasov <79853886592@yandex.ru>
2023-02-06 18:16:22 +0300

    Added CRUD operations for "User"

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 ﻿using API.Models;
+using AutoMapper;
 using BlogWebApp.BLL.Services;
 using BlogWebApp.BLL.Services.Interfaces;
 using BlogWebApp.BLL.ViewModels.Users;
+using BlogWebApp.DAL.Extentions;
 using BlogWebApp.DAL.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -28,7 +30,7 @@ namespace API.Controllers
             _userManager = userManager;
         }
 
-        [HttpPost, Route("login")]
+        [HttpPost, Route("Login")]
         public async Task<IActionResult> Login(Login login)
         {
             try
@@ -67,7 +69,7 @@ namespace API.Controllers
             return Unauthorized();
         }
 
-        [HttpPost, Route("register")]
+        [HttpPost, Route("Register")]
         public async Task<ActionResult<User>> Register(Register register)
         {
             if (ModelState.IsValid)
@@ -117,6 +119,128 @@ namespace API.Controllers
             }
 
             return BadRequest("У пользователя нет необходимой роли.");
+        }
+
+        [HttpGet("Get"), Authorize]
+        public async Task<ActionResult<UserViewModel>> Get(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return BadRequest("Ошибка получения данных пользователя из БД.");
+            }
+
+            var model = await _userService.GetUserViewModelAsync(user);
+            return Ok(model);
+        }
+
+        [HttpPost("Add"), Authorize]
+        public async Task<ActionResult<User>> Add(Register register)
+        {
+            var user = await _userManager.FindByEmailAsync(User?.Identity?.Name);
+            if (user == null)
+            {
+                return BadRequest("Ошибка получения данных пользователя из БД.");
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            if (userRoles == null)
+            {
+                return BadRequest("Ошибка получения данных пользователя из БД.");
+            }
+
+            if (userRoles.FirstOrDefault(o => o.Equals("Администратор")) == null)
+            {
+                return BadRequest("У пользователя нет необходимой роли.");
+            }
+            
+            if (ModelState.IsValid)
+            {
+                var newUser = new User
+                {
+                    UserName = register.Email,
+                    Email = register.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(newUser, register.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(newUser, "пользователь");
+
+                    return Ok(newUser);
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return BadRequest("An error occurred while registering the user");
+        }
+
+        [HttpPatch("Update"), Authorize]
+        public async Task<IActionResult> Update(UserEditViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            user.Convert(model);
+
+            if (!await _userService.UpdateAsync(user, model.Email))
+            {
+                return BadRequest("Ошибка обновления данных пользователя '{model.Email}'.");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPatch("UpdatePassword"), Authorize]
+        public async Task<IActionResult> UpdatePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(User?.Identity?.Name);
+                if (user == null)
+                {
+                    return BadRequest("Ошибка получения данных пользователя из БД.");
+                }
+
+                var changePasswordResult = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                if (!changePasswordResult.Succeeded)
+                {
+                    foreach (var error in changePasswordResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    return BadRequest("Ошибка при изменении пароля пользователя.");
+                }
+
+                await _signInManager.RefreshSignInAsync(user);
+                return Ok(user);
+            }
+
+            return BadRequest("Ошибка при изменении пароля пользователя.");
+        }
+
+        [HttpDelete("Delete"), Authorize]
+        public async Task<IActionResult> Delete(UserDeleteViewModel model)
+        {
+            var userId = model.Id;
+            var user = await _userService.FindByIdAsync(userId);
+            if (user != null)
+            {
+                if (!await _userService.DeleteAsync(user, model))
+                {
+                    return BadRequest("Ошибка удаления пользователя.");
+                }
+            }
+            else
+            {
+                return BadRequest($"Не найден пользователь с ID '{userId}'.");
+            }
+
+            return Ok("Пользователь успешно удален.");
         }
     }
 }
